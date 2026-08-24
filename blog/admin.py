6f95347e8e532cadcb0ecea0243d51d75bbf8999
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-24 11:05:52 +0300

    Added more post options to views

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-class PostAdmin(Admin.ModelAdmin): 
+class PostAdmin(admin.ModelAdmin): 
     list_display = (
         "title",
         "author",
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+
+def post_list(request):
+    posts = posts.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+
