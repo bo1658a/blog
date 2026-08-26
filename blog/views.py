182e4cbf64f87548e01b337d0303657bfd922fc0
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-26 12:42:47 +0300

    updated view with return/request

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -6,5 +6,10 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_detail.html", {"post": post})
 
 
