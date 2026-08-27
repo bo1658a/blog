75004cbf54fd24c07d0592426467691b7c3a49cf
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-27 11:33:08 +0300

    Updated views with class-based

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "post_detail.html", {"post": post})
-
-
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
