8d3a5e5cabd1729a382306518b24c37af88811c2
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-27 13:04:07 +0300

    created delete view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 from .models import Post
@@ -29,4 +30,9 @@ class BlogUpdateView(UpdateView):
     template_name = "post_edit.htnl"
     fields = ["title", "body"]
 
-    
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
