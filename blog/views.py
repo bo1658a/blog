e3894bf052f56b17100d6fc78e0a5ce43f2f6b87
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-27 12:40:40 +0300

    defined model, template name and fields

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+
+
 from .models import Post
 
 
@@ -19,3 +21,12 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.htnl"
+    fields = ["title", "body"]
+
+    
