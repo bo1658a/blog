1c7260ef2cb03498f200b973d7a47d099abcbb84
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-27 13:46:06 +0300

    updated urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,12 @@ from .views import (
     BlogCreateView,
     BlogUpdateView,
     BlogDeleteView,
-)  
+)
 
 urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
-    path("post/<int:pk>/delete/", BlogDeleteView.as_view(),
-        name="post_delete"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home"),
 ]
