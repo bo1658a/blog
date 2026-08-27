f116bfa5676e92d5a995084dffb53c3f99395db6
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-08-27 14:05:16 +0300

    cleared bugs and typos

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "blog",
 ]
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
