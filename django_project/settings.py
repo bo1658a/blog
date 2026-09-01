c660b98b5bc4f6b1ed3a85635e9b098021fa485a
Bob Brhayan Okoth <brhayan90@gmail.com>
2026-09-01 14:05:54 +0300

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
     "accounts",
     "pages",
     "articles",
+    "blog",
     
 ]
 
@@ -136,5 +142,14 @@ STORAGES = {
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+# Email
+# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
+
+MAILERS = {
+    "default": {
+        "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
